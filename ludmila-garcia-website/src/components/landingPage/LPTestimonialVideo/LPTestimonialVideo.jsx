ad30c9ae5ad4ad67821108300a4760dff9effa42
Gilberto Sá <gibasa@gmail.com>
2025-05-08 09:02:34 -0300

    new testimonial video added

--- ludmila-garcia-website/src/components/landingPage/LPTestimonialVideo/LPTestimonialVideo.jsx
+++ ludmila-garcia-website/src/components/landingPage/LPTestimonialVideo/LPTestimonialVideo.jsx
@@ -3,15 +3,24 @@ import "./LPTestimonialVideo.css";
 function LPTestimonialVideo() {
   return (
     <div className="lp-testimonial-container">
-      <h3 className="lp-testimonial-title">Veja o que diz nossa aluna:</h3>
-      
-      <div className="lp-video-testimonial-wrapper">
-        <video 
-          src="/images/depoimento-lud.mp4"
-          className="lp-testimonial-video"
-          controls
-          playsInline
-        />
+      <h3 className="lp-testimonial-title">Veja o que dizem nossas alunas:</h3>
+      <div className="lp-testimonial-videos">
+        <div className="lp-video-testimonial-wrapper">
+          <video
+            src="/images/depoimento-lud.mp4"
+            className="lp-testimonial-video"
+            controls
+            playsInline
+          />
+        </div>
+        <div className="lp-video-testimonial-wrapper">
+          <video
+            src="/images/depoimento-lud 2.mp4"
+            className="lp-testimonial-video"
+            controls
+            playsInline
+          />
+        </div>
       </div>
     </div>
   );
